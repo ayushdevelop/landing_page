dc57067a29866b6d79d4504142fed0616921117f
Ayush Srivastava <apple@apples-MacBook-Air.local>
2022-08-01 15:32:18 +0530

    added styles to navigation bar, and rendered dynamic data

--- src/pages/components/Navigation.js
+++ src/pages/components/Navigation.js
@@ -1,9 +1,19 @@
 import React from "react";
+import { useStaticQuery, graphql } from "gatsby";
 
 const Navigation = () => {
+  const data = useStaticQuery(graphql`
+    query {
+      strapiComponentLogoLogo {
+        Logo_title
+      }
+    }
+  `);
+  const logoTitle = data.strapiComponentLogoLogo.Logo_title;
+
   return (
     <div className="max-w-7xl bg-white h-5 p-6 m-auto flex justify-items-center">
-      <div className="flex-auto text-2xl">Codemancers</div>
+      <div className="flex-auto text-2xl">{logoTitle}</div>
     </div>
   );
 };
