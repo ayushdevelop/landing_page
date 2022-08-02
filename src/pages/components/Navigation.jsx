de92d80c7f089abd9340dda9f22fa4814e56bc5e
Ayush Srivastava <apple@apples-MacBook-Air.local>
2022-08-02 18:44:25 +0530

    added logo in the navbar component

--- src/pages/components/Navigation.jsx
+++ src/pages/components/Navigation.jsx
@@ -2,10 +2,13 @@ import React from "react";
 
 const Navigation = ({navBarContent}) => {
   const {logo_title, logo_image} = navBarContent;
+  const [imgSrc] = logo_image; // image url source
+  const src = imgSrc.localFile.url
 
 return (
     <div className="max-w-7xl bg-white h-5 p-6 m-auto flex items-center">
-      <div className="ml-20 mt-10 text-2xl text-gray-500">{logo_title}</div>
+      <img src={src} className="ml-20 mt-10" style={{width:"50px", height:"50px"}} />
+      <div className="ml-2 mt-10 text-2xl text-gray-500">{logo_title}</div>
     </div>
   );
 };
