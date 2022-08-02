60c2ed588a9cd0057cb396688e616a15db3d766c
Ayush Srivastava <apple@apples-MacBook-Air.local>
2022-08-02 07:17:09 +0530

    added multiple plugins

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,7 +1,33 @@
+require("dotenv").config({
+  path: `.env.${process.env.NODE_ENV}`,
+});
+
+const strapiConfig = {
+  apiURL: process.env.STRAPI_API_URL,
+  accessToken: process.env.STRAPI_TOKEN,
+  collectionTypes: ["page"],
+  singleTypes: [],
+};
+
 module.exports = {
   siteMetadata: {
     title: `Landing Page Application`,
     siteUrl: `https://www.yourdomain.tld`,
   },
-  plugins: ["gatsby-plugin-postcss"],
+  plugins: [
+    "gatsby-plugin-postcss",
+    "gatsby-plugin-image",
+    "gatsby-plugin-sharp",
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: `pages`,
+        path: `${__dirname}/gatsby-config.js`,
+      },
+    },
+    {
+      resolve: `gatsby-source-strapi`,
+      options: strapiConfig,
+    },
+  ],
 };
