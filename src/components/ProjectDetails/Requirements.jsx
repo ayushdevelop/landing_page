0dc668c719d2ab0ea805b33d5ea2473250117da3
Ayush Srivastava <apple@apples-MacBook-Air.local>
2022-08-04 13:22:00 +0530

    changed alignment to adjust with responsiveness of website

--- src/components/ProjectDetails/Requirements.jsx
+++ src/components/ProjectDetails/Requirements.jsx
@@ -5,7 +5,7 @@ const Requirements = ({project}) => {
   return (
     <div className="mt-2 mb-2 p-2 flex flex-col items-center sm:items-start">
       <div className="font-medium text-gray-900 mb-1">Requirements</div>
-      <div className="text-sm text-gray-600">
+      <div className="text-xs sm:text-sm text-center sm:text-left text-gray-600">
         {requirements}
       </div>
     </div>
